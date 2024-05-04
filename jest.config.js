59417f2c5d7a8c8598f8837f8f2ccac87f627ad3
Daniel Gutkin <dngutkin@gmail.com>
2024-05-03 21:38:30 -0400

    Setup jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+
+// export default {
+//   clearMocks: true,
+//   coverageProvide: "v8",
+//   moduleFileExtension: ["js", "jsx", "ts", "tsx", "json"],
+//   roots: ["<rootDir>/src"],
+//   testMatch: ["**/__tests__/**/*.[jt]s?(x)"]
+// }
