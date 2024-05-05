d3bc6282c974e1d87a8c9d8fff6caff1aa7b3e10
Daniel Gutkin <dngutkin@gmail.com>
2024-05-05 06:16:06 -0400

    Remove comments from jest config

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,3 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
-
-// export default {
-//   clearMocks: true,
-//   coverageProvide: "v8",
-//   moduleFileExtension: ["js", "jsx", "ts", "tsx", "json"],
-//   roots: ["<rootDir>/src"],
-//   testMatch: ["**/__tests__/**/*.[jt]s?(x)"]
-// }
